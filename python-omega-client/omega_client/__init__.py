ab1fb1f00cf12b30d57c5275e0a0bc20aa0b74d6
Ethan Chu <xychu2008@gmail.com>
2016-04-11 15:43:41 +0800

    Raise exception when got unecpected return code.

--- python-omega-client/omega_client/__init__.py
+++ python-omega-client/omega_client/__init__.py
@@ -5,6 +5,24 @@ import requests
 BASE_URL = '/api/v3'
 
 
+class OmegaException(Exception):
+    pass
+
+
+def check_return_code(function):
+    def with_code_check(*args, **kwargs):
+        # raise exception when omega get code big than zero;
+        try:
+            rst = function(*args, **kwargs)
+            if 'code' in rst and rst['code'] > 0:
+                raise OmegaException("Calls to Omega got unexpected result: %s" % rst)
+            return rst
+        except Exception as e:
+            raise OmegaException(e)
+
+    return with_code_check
+
+
 class OmegaClient(object):
 
     def __init__(self, server_url, email, password):
@@ -29,41 +47,54 @@ class OmegaClient(object):
     def __getitem__(self, *args, **kwargs):
         return self.client.__getitem__(*args, **kwargs)
 
+    @check_return_code
     def get_auth(self):
         return GET(self.client.auth)
 
+    @check_return_code
     def post_auth(self, email, password):
         return POST(self.client.auth, {'email': email, 'password': password})
 
+    @check_return_code
     def get_clusters(self):
         return GET(self.client.clusters)
 
+    @check_return_code
     def post_clusters(self, **kwargs):
         return POST(self.client.clusters, kwargs)
 
+    @check_return_code
     def get_cluster(self, cluster_id):
         return GET(self.client.clusters[cluster_id])
 
+    @check_return_code
     def delete_cluster(self, cluster_id):
         return DELETE(self.client.clusters[cluster_id])
 
+    @check_return_code
     def get_node_identifier(self, cluster_id):
         return GET(self.client.clusters[cluster_id].new_node_identifier)
 
+    @check_return_code
     def get_apps(self, cluster_id):
         return GET(self.client.clusters[cluster_id].apps)
 
+    @check_return_code
     def post_apps(self, cluster_id, **kwargs):
         return POST(self.client.clusters[cluster_id].apps, kwargs)
 
+    @check_return_code
     def get_app(self, cluster_id, app_id):
         return GET(self.client.clusters[cluster_id].apps[app_id])
 
+    @check_return_code
     def delete_app(self, cluster_id, app_id):
         return DELETE(self.client.clusters[cluster_id].apps[app_id])
 
+    @check_return_code
     def post_nodes(self, cluster_id, **kwargs):
         return POST(self.client.clusters[cluster_id].nodes, kwargs)
 
+    @check_return_code
     def delete_nodes(self, cluster_id, *args):
         return DELETE(self.client.clusters[cluster_id].nodes, args)
